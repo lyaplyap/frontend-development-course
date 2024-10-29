6e45bc8f6c72fbff514fefd6b7376cbbf646080a
eznova <e_znova@mail.ru>
2024-10-29 22:22:04 +0300

    Task 10 прошел тест
    mend

--- sandbox/src/task10-interview-task/index.js
+++ sandbox/src/task10-interview-task/index.js
@@ -1,10 +1,20 @@
-
 /**
  * Возвращает одноразрядное число, полученное в результате итеративного перемножения всех цифр числа
  * 
  * @param {Number} num
  * @returns {Number}
  */
+
 export const solutionFn = (num) => {
-    // Ваш код здесь
+    // Перемножаем цифры числа пока оно не станет одноразрядным
+    while (num >= 10) {
+        let product = 1;
+        while (num > 0) {
+            product *= num % 10;
+            num = Math.floor(num / 10);
+        }
+        num = product;
+    }
+    // Возвращаем одноразрядное число
+    return num;
 };
