0e14cc17bf2b0a230cad3a0656e36fce32ce8e57
Elizabeth Znova <e_znova@mail.ru>
2024-10-29 20:56:45 +0300

    task1 проходит тест
    mend

--- sandbox/src/task1-even-calc/index.js
+++ sandbox/src/task1-even-calc/index.js
@@ -5,5 +5,9 @@
  * @returns {number} Сумма чётных чисел
  */
 export const solutionFn = (numbers) => {
-    // Ваш код здесь
+    // reduce для суммы всех чётных чисел
+    return numbers.reduce((collector, current) => {
+        // Проверка на чётность
+        return current % 2 === 0 ? collector + current : collector;
+    }, 0); // Начальное значение collector — 0
 };
