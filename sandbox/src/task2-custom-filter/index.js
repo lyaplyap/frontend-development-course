a03c94f066c20a4142ec76c17405b85cfc766d36
eznova <e_znova@mail.ru>
2024-10-29 21:55:46 +0300

    Task 2 прошел тест
    mend

--- sandbox/src/task2-custom-filter/index.js
+++ sandbox/src/task2-custom-filter/index.js
@@ -3,9 +3,29 @@
  * 
  * @param {Array} array Массив
  * @param {(...args: any[]) => Boolean} filterFn Фильтрующая булева функция (та, которая в качестве результата возвращает true или false)
- * @param {Boolean} inplace Флаг "модифицируем исходный массив или нет", 
+ * @param {Boolean} inplace Флаг "модифицируем исходный массив или нет"
  * @returns {Array} Отфильтрованный массив
  */
 export const filter = (array, filterFn, inplace = false) => {
-    // Ваш код здесь
+    // Если inplace=true, создаем новый массив
+    const resArr = inplace ? [] : [];
+
+    // Проходимся по исходному массиву
+    for (let i = 0; i < array.length; i++) {
+        // Если элемент удовлетворяет условию, добавляем его в отфильтрованный массив
+        if (filterFn(array[i], i, array)) {
+            resArr.push(array[i]);
+        }
+    }
+
+    // Если inplace=true, модифицируем исходный массив, убирая из него лишние элементы
+    if (inplace) {
+        // Очистим исходный массив и добавим в него только отфильтрованные элементы
+        array.length = 0;
+        resArr.forEach(element => array.push(element));
+        return array;
+    }
+
+    // Вернем отфильтрованный массив
+    return resArr;
 };
